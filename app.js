cf92da3ceeace755c0b37d85abeb30c031299ec6
Headonnn <[th.couraud@gmail.com](th.couraud@gmail.com)>
2023-05-09 11:03:09 +0200

    quete 3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const user = process.env.DB_USER
 const app = express();
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -18,7 +19,8 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandler.getUser);
 app.get("/api/users/:id", userHandler.getUserById);
-
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", userHandler.postUser);
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
